8e1dc85c8522e880da498b9e47361c695765aa9b
朝霧 <102600436+yaoting0116@users.noreply.github.com>
2025-03-09 17:57:40 +0800

    Add files via upload

---
+++ MIMIC_IV_pages/Introduction.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show():
+    st.title("MIMIC IV 2.1 Introduction")
+    #st.write("這個網頁提供了MIMIC IV 2.1的需要用到關於特定疾病和特定藥物的資料表，以方便各位在針對類似的學術研究時更好的了解，這些醫療資料的欄位資訊，我們能夠透過選單來切換到使用醫療資料時的SQL範例，此範例僅供參考和學術研究。")
+    st.write("This webpage provides the necessary MIMIC IV 2.1 data tables related to specific diseases and medications. It is designed to help you better understand the column information in these medical datasets for similar academic research. You can use the menu to switch to SQL examples for querying the medical data. Please note that these examples are for reference and academic research purposes only.")
